4032d38f28a7b0b8b9f92d6bf18e823c46579d62
ZhengMaster2020 <2105195274@qq.com>
2019-09-24 23:42:59 +0800

    update

--- src/views/ShowEdit.vue
+++ src/views/ShowEdit.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="document">
     <Header/>
+    <el-row class="main">
+      <el-col :span="20">
+        content
+      </el-col>
+      <el-col :span="4">
+        <el-button>生成PDF文档</el-button>
+        <el-button>下载</el-button>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -13,3 +22,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.main {
+  text-align: center;
+  margin: 20px;
+  padding: 10px;
+}
+</style>
